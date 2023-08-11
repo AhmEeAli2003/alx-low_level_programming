549a97b73e3230bc2870b33ad916308b89e16b3b
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-11 16:49:27 -0700

    Add file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
