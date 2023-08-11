4d699bebda2ce52e73efaf0b1d39a249e87f371c
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-10 19:31:44 -0700

    Edit style of 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  *
  * Return: Always 1 (Success)
  */
-int main() {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora \
+Korpar, 2015-10-19\n";
 write(2, message, 59);
 return (1);
 }
