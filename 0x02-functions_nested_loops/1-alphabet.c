e680ac29203d1890248f6e7c86ae7ac8d63669b7
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-15 14:28:23 -0700

    Edit fil e1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,6 @@
 void print_alphabet(void);
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
-/**
  * print_alphabet - print alphabet in small case
  *
  * Return: Always void.
