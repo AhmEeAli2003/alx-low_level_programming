18325d20faa93dca84a9aeb365a95281c5958e3f
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-15 17:22:20 -0700

    Edit file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int c)
 	char r;
 
 	r = c > 0 ? c <= 9 ? (c) : (c % 10) :
-		-c <= 9 ? (-c) : (-c % 10);
+		-c <= 9 ? (-c) : ((c % 10) * -1);
 	_putchar(r + '0');
 	return (r);
 }
