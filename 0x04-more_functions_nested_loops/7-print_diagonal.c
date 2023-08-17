94207169f81a7c9e453e69ea982315068ab87adf
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 14:56:45 -0700

    Edit file 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,25 @@
  */
 void print_diagonal(int n)
 {
+	int space = 0, temp;
+
 	if (n > 0)
 	{
 		while (n--)
 		{
+			temp = space;
+			while (temp > 0)
+			{
+				_putchar(' ');
+				temp--;
+			}
+			space++;
 			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
