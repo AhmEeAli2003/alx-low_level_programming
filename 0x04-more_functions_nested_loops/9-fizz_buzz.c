c812c5ede3c852473646649b5c73e35ab5c64fe4
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 16:25:48 -0700

    Hot Edit file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 	if (i % 5 == 0 && i % 3 == 0)
 		printf("FizzBuzz ");
@@ -21,6 +21,7 @@ for (i = 1; i <= 100; i++)
 	else
 		printf("%d ", i);
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
