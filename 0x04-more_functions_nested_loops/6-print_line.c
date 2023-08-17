2279191c035d780e26870033775c5f9266551af5
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 14:17:54 -0700

    Edit file 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,7 @@
 
 /**
  * print_line - print line
+ * @n: number of unders scores.
  *
  * Return: Always void.
  */
@@ -13,6 +14,6 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-	}	
+	}
 	_putchar('\n');
 }
