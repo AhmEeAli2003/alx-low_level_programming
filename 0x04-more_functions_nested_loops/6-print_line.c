f96789cce0e96f288a79a748bc89f766355851c8
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 14:07:53 -0700

    add file 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *
+ * Return: Always void.
+ */
+void print_line(int n)
+{
+	if(n > 0)
+	{
+		while(n--)
+		{
+			_putchar('_');
+		}
+	}
+	
+	_putchar('\n');
+}
