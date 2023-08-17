6ee86250721d4c6d72dce4eb0e50223eca34e14e
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 16:12:18 -0700

    Edit file 9 v.1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,21 @@ for (i = 1; i <= 100; i++)
 {
 	if (i % 15 == 0)
 	{
-		printf("FizzBuzz");
+		printf("FizzBuzz ");
 	}
 	else if (i % 5 == 0)
 	{
-		printf("Buzz");
+		printf("Buzz ");
 	}
 	else if (i % 3 == 0)
 	{
-		printf("Fizz");
+		printf("Fizz ");
 	}
 	else
 	{
-		printf("%d", i);
+		printf("%d ", i);
 	}
-	printf(" ");
 }
+printf("\b\n");
 return (0);
 }
