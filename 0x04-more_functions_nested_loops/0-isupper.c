abd93ec287d0f4d5b6346501a301b7ef34b6d048
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-17 13:07:17 -0700

    Add file 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check if number is upper
+ * @c: letter
+ *
+ * Return: 1 if c is uppercase.
+ * 0 otherwise.
+ */
+int _isupper(int c);
+{
+	return (c >= 65 && c <= 90 ? (1) : (0));
+}
