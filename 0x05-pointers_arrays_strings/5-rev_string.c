0a831f960e66a0324f043acba6bb96cbb6d42492
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-21 15:19:37 -0700

    Add file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse string.
+ * @s: pointer to char
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char temp[100];
+int len = 0, i, j = 0;
+
+for (i = 0; ; i++)
+{
+	if(s[i] == '\0')
+		break;
+	temp[i] = s[i];
+	len++;
+}
+
+for (i = len - 1; i >= 0; i--, j++)
+{
+	s[j] = temp[i]; 
+}
+}
