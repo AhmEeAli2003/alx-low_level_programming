193c2f5500c25425e0021833eee68170baee419f
Ahmed Ali <bdalftahahmd911@gmail.com>
2023-08-21 15:59:20 -0700

    Edit file 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,16 +10,11 @@ void puts_half(char *s)
 {
 int i = 0, len = 0, half = 0;
 
-for (len = 0 ; ; len++)
-	if (s[len] == '\0')
-		break;
+for (len = 0 ; s[len] != '\0' ; len++)
+	;
 half = len % 2 == 1 ? (len - 1) / 2 : len / 2;
 
-for (i = half ; ; i++)
-{
-	if (s[i] == '\0')
-		break;
+for (i = half ; s[i] != '\0' ; i++)
 	_putchar(s[i]);
-}
 _putchar('\n');
 }
